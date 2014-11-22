d436062e04054edef88941c82bbd01184bf62572
masyan <jmasyan@gmail.com>
2014-11-22 13:07:06 +0500

    gitpython version

--- recipes/cicd.rb
+++ recipes/cicd.rb
@@ -1,16 +1,21 @@
-package 'python-dev' do
+package "python-dev" do
   action :install
 end
 
-package 'libmysqlclient-dev' do
+package "libmysqlclient-dev" do
   action :install
 end
 
-python_pip 'mysql-connector-python' do
-  options '--allow-external mysql-connector-python'
+python_pip "mysql-connector-python" do
+  options "--allow-external mysql-connector-python"
   action :install
 end
 
-python_pip 'jtalks-cicd' do
+python_pip "jtalks-cicd" do
   action :install
+end
+
+python_pip "GitPython" do
+  action :install
+  version "0.3.2.RC1"
 end
