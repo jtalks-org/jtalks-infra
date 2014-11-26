dd5e128a8ea805071a91818147ce43a18d3c0ee8
masyan <jmasyan@gmail.com>
2014-11-26 21:00:14 +0500

    clean code

--- definitions/restart_service.rb
+++ definitions/restart_service.rb
@@ -1,5 +1,5 @@
 # create resource execute to restart service by {user}
-define :restart_service, :user => 'root', :ssh_dir => '.ssh', :key_name => 'id_rsa', :source_key_dir => nil do
+define :restart_service, :user => 'root' do
 
   execute "#{params[:name]}_restart" do
     command "su - #{params[:user]} -c 'service #{params[:name]} restart'"
