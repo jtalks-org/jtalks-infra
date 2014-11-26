67d2761ca3ffe8ba6fa4839deac781f4c7153d50
masyan <jmasyan@gmail.com>
2014-11-26 20:59:54 +0500

    new definition to start service by user

--- definitions/restart_service.rb
+++ definitions/restart_service.rb
@@ -1,7 +1,7 @@
 # create resource execute to restart service by {user}
 define :restart_service, :user => 'root', :ssh_dir => '.ssh', :key_name => 'id_rsa', :source_key_dir => nil do
 
-  execute "#{param[:name]}_restart" do
+  execute "#{params[:name]}_restart" do
     command "su - #{params[:user]} -c 'service #{params[:name]} restart'"
     action :nothing
   end
