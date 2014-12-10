1fee3589ac8d14f290cd5222dc05fbff18d7ea7a
masyan <jmasyan@gmail.com>
2014-12-10 20:56:08 +0500

    generate config file to fisheye, without block og trusted applications

--- providers/fisheye.rb
+++ providers/fisheye.rb
@@ -22,13 +22,26 @@ def load_current_resource
   @current_resource.user(@new_resource.user)
   @current_resource.data_dir(@new_resource.data_dir)
   @current_resource.port(@new_resource.port)
+  @current_resource.host(@new_resource.host)
+  @current_resource.control_port(@new_resource.control_port)
+  @current_resource.crowd_url(@new_resource.crowd_url)
   @current_resource.crowd_app_name(@new_resource.crowd_app_name)
   @current_resource.crowd_app_password(@new_resource.crowd_app_password)
+  @current_resource.crowd_groups(@new_resource.crowd_groups)
+  @current_resource.crowd_admin_group(@new_resource.crowd_admin_group)
   @current_resource.db_name(@new_resource.db_name)
   @current_resource.db_user(@new_resource.db_user)
   @current_resource.db_password(@new_resource.db_password)
   @current_resource.db_backup_path(@new_resource.db_backup_path)
   @current_resource.version(@new_resource.version)
+  @current_resource.license_text(@new_resource.license_text)
+  @current_resource.crucible_license_text(@new_resource.crucible_license_text)
+  @current_resource.git_bin_path(@new_resource.git_bin_path)
+  @current_resource.smtp_host(@new_resource.smtp_host)
+  @current_resource.smtp_port(@new_resource.smtp_port)
+  @current_resource.smtp_user(@new_resource.smtp_user)
+  @current_resource.smtp_password(@new_resource.smtp_password)
+  @current_resource.repositories(@new_resource.repositories)
 
   if Pathname.new("/home/#{@new_resource.user}/#{@current_resource.service_name}").exist?
     @current_resource.exists = true
@@ -38,10 +51,59 @@ end
 def prepare
   user = "#{current_resource.user}"
   data_dir = "#{current_resource.data_dir}"
-
+  control_port = current_resource.control_port
+  http_port = current_resource.port
+  fisheye_license = "#{current_resource.license_text}"
+  crusible_license = "#{current_resource.crucible_license_text}"
+  git_bin_path = "#{current_resource.git_bin_path}"
+  url = "#{current_resource.host}"
+  smtp_host = "#{current_resource.smtp_host}"
+  smtp_port = "#{current_resource.smtp_port}"
+  smtp_user = "#{current_resource.smtp_user}"
+  smtp_password = "#{current_resource.smtp_password}"
+  crowd_url = "#{current_resource.crowd_url}"
+  crowd_app_name = "#{current_resource.crowd_app_name}"
+  crowd_app_password = "#{current_resource.crowd_app_password}"
+  crowd_groups = current_resource.crowd_groups
+  crowd_admin_group = "#{current_resource.crowd_admin_group}"
+  repositories = current_resource.repositories
+  db_name = "#{current_resource.db_name}"
+  db_password = "#{current_resource.db_password}"
+  db_user = "#{current_resource.db_user}"
+  
   directory "#{data_dir}" do
     owner user
     group user
+    recursive true
+    notifies :run, "execute[#{current_resource.service_name}_restart]", :delayed
+  end
+
+  template "#{data_dir}/config.xml" do
+    source 'fisheye.config.xml.erb'
+    mode '775'
+    owner user
+    group user
+    variables({
+                  :control_port => control_port,
+                  :fisheye_license => fisheye_license,
+                  :crusible_license => crusible_license,
+                  :git_bin_path => git_bin_path,
+                  :url => url,
+                  :http_port => http_port,
+                  :smtp_host => smtp_host,
+                  :smtp_port => smtp_port,
+                  :smtp_user => smtp_user,
+                  :smtp_password => smtp_password,
+                  :crowd_url => crowd_url,
+                  :crowd_app_name => crowd_app_name,
+                  :crowd_app_password => crowd_app_password,
+                  :crowd_groups => crowd_groups,
+                  :crowd_admin_group =>crowd_admin_group ,
+                  :repositories => repositories,
+                  :db_name => db_name,
+                  :db_password => db_password,
+                  :db_user => db_user
+              })
     notifies :run, "execute[#{current_resource.service_name}_restart]", :delayed
   end
 
--- resources/fisheye.rb
+++ resources/fisheye.rb
@@ -6,13 +6,26 @@ attribute :service_name       , :name_attribute => true, :required => true,  :ki
 attribute :data_dir       , :required => true,  :kind_of => String
 attribute :user       , :required => true,  :kind_of => String
 attribute :source_url       , :required => true,  :kind_of => String
+attribute :host       , :required => true,  :kind_of => String
 attribute :port       , :required => true,  :kind_of => Integer
+attribute :control_port       , :required => true,  :kind_of => Integer
+attribute :crowd_url       , :required => true,  :kind_of => String
 attribute :crowd_app_name       , :required => true,  :kind_of => String
 attribute :crowd_app_password       , :required => true,  :kind_of => String
+attribute :crowd_groups       , :required => true,  :kind_of => Array
+attribute :crowd_admin_group     , :required => true, :kind_of => String
 attribute :db_name     , :required => true, :kind_of => String
 attribute :db_user     , :required => true, :kind_of => String
 attribute :db_password     , :required => true, :kind_of => String
 attribute :db_backup_path     , :required => true, :kind_of => String
 attribute :version     , :required => true, :kind_of => String
+attribute :license_text     , :required => true, :kind_of => String
+attribute :crucible_license_text     , :required => true, :kind_of => String
+attribute :git_bin_path     , :required => true, :kind_of => String
+attribute :smtp_host     , :required => true, :kind_of => String
+attribute :smtp_port     , :required => true, :kind_of => String
+attribute :smtp_user     , :required => true, :kind_of => String
+attribute :smtp_password     , :required => true, :kind_of => String
+attribute :repositories     , :required => true, :kind_of => Hash
 
 attr_accessor :exists
