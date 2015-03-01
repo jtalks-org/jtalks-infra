240c9126e7a0b292f8ae17cfdeb16212d02e0670
masyan <jmasyan@gmail.com>
2015-03-01 14:07:17 +0500

    fix definition to backup stable version

--- definitions/stable_backup.rb
+++ definitions/stable_backup.rb
@@ -1,4 +1,5 @@
 define :stable_backup, :user => nil, :service_name => "", :tomcat_version => "8",:version => nil, :db_name => nil, :db_user => nil, :db_pass => nil, :paths => [] do
+  init_dir = "#{node[:jtalks][:path][:init_script]}"
   file_separator = "/"
   user = params[:user]
   service_name = params[:service_name]
@@ -32,44 +33,53 @@ define :stable_backup, :user => nil, :service_name => "", :tomcat_version => "8"
 
       if [ ! "#{version}" = $version  ]; then
           echo 'New Version, need update'
-          rm -Rf current_*
-          touch "current_#{version}"
           rm -Rf "#{backup_dir}/stable"
           mkdir "#{backup_dir}/stable"
+          cp current_* "#{backup_dir}/stable"
+          rm -Rf current_*
+          touch "current_#{version}"
+
           touch "#{backup_dir}/stable/restore"
           chmod +x "#{backup_dir}/stable/restore"
-          echo '#/bin/bash \r\n' >> #{backup_dir}/stable/restore
+          echo '#/bin/bash \n' >> #{backup_dir}/stable/restore
 
           echo 'echo "Stopping service"' >> #{backup_dir}/stable/restore
-          echo 'service #{service_name} stop' >> #{backup_dir}/stable/restore
-          echo 'pkill -9 -f #{service_name} \r\n\r\n' >> #{backup_dir}/stable/restore
+          echo '#{init_dir}/#{service_name} stop' >> #{backup_dir}/stable/restore
+          echo 'pkill -9 -f #{service_name} \n\n' >> #{backup_dir}/stable/restore
 
           #if db_name set do backup
            if [ ! ''#{db_name}'' = '' ]; then
               mysqldump -u #{db_user} --password=\"#{db_pass}\" --opt  #{db_name} >  #{backup_dir}/stable/db.sql
-              echo '#Restore db \r\n' >>  #{backup_dir}/stable/restore
+              echo '#Restore db \n' >>  #{backup_dir}/stable/restore
               echo 'mysql -u #{db_user} --password=\"#{db_pass}\" -D #{db_name} -e \"drop database #{db_name}; create database #{db_name}; \" '  >>  #{backup_dir}/stable/restore
-              echo 'mysql -u #{db_user} --password=\"#{db_pass}\" -D #{db_name}  < #{backup_dir}/stable/db.sql \r\n\r\n'  >>  #{backup_dir}/stable/restore
+              echo 'mysql -u #{db_user} --password=\"#{db_pass}\" -D #{db_name}  < #{backup_dir}/stable/db.sql \n\n'  >>  #{backup_dir}/stable/restore
            fi
 
-         echo '#Restore files \r\n' >>  #{backup_dir}/stable/restore
+         echo '#Restore files \n' >>  #{backup_dir}/stable/restore
           for p in ${paths[@]}
           do
+             if [ $(basename $p) = "#{service_name}" ]; then
+               p=$(echo $p | sed -r 's/#{service_name}$/apache-tomcat-#{node[:tomcat][tomcat_version][:version]}/g')
+             fi
              cp -LR $p #{backup_dir}/stable
-             echo  'cp -LR #{backup_dir}/stable/'$(basename $p) $p >> #{backup_dir}/stable/restore
+             echo  'rm -R ' $p >> #{backup_dir}/stable/restore
+             echo  'cp -R #{backup_dir}/stable/'$(basename $p) $p >> #{backup_dir}/stable/restore
              echo  'echo "Restored ' $p '"' >> #{backup_dir}/stable/restore
              echo 'chown -R #{user}.#{user} '$p  >> #{backup_dir}/stable/restore
-             echo  'echo "Set owner to ' $p '" \r\n' >> #{backup_dir}/stable/restore
+             echo  'echo "Set owner to ' $p '" \n' >> #{backup_dir}/stable/restore
           done
 
            if [ ! "#{service_name}" = "" ]; then
                echo 'rm -Rf /home/#{user}/#{service_name}'  >> #{backup_dir}/stable/restore
-               echo 'ln -s /home/#{user}/apache-tomcat-#{node[:tomcat][tomcat_version][:version] } #{service_name}'  >> #{backup_dir}/stable/restore
-               echo  'echo "Recreated link" \r\n' >> #{backup_dir}/stable/restore
+               echo 'ln -s /home/#{user}/apache-tomcat-#{node[:tomcat][tomcat_version][:version]} /home/#{user}/#{service_name}'  >> #{backup_dir}/stable/restore
+               echo  'echo "Recreated link" \n' >> #{backup_dir}/stable/restore
           fi
 
           echo 'echo "Starting service"' >> #{backup_dir}/stable/restore
-          echo 'service #{service_name} start' >> #{backup_dir}/stable/restore
+          echo '#{init_dir}/#{service_name} start' >> #{backup_dir}/stable/restore
+
+          echo 'rm -Rf #{backup_dir}/current*' >> #{backup_dir}/stable/restore
+          echo 'cp -f #{backup_dir}/stable/current_* #{backup_dir}'  >> #{backup_dir}/stable/restore
       fi
     EOH
   end
