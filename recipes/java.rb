ddddc4ef64120bebc62f520675eb2be8326ce6aa
masyan <jmasyan@gmail.com>
2015-07-17 10:55:20 +0500

    install multiple jdks

--- recipes/java.rb
+++ recipes/java.rb
@@ -5,5 +5,6 @@ java_ark "oracle-jdk8-x86_64" do
   app_home '/usr/lib/jvm/jdk1.8.0_51'
   default false
   alternatives_priority 1
+  bin_cmds node[:java][:jdk]["8"][:bin_cmds]
   action :install
 end
